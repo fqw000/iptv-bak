e253bff01b1c7085749aab960a49d6e353b53ab5
Zephyr <wang_zi_mo@163.com>
2024-08-27 22:13:45 +0800

    Create fofa.sh

---
+++ speedtest/fofa.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# cd /root/iptv
+# read -p "确定要运行脚本吗？(y/n): " choice
+
+# 判断用户的选择，如果不是"y"则退出脚本
+# if [ "$choice" != "y" ]; then
+#     echo "脚本已取消."
+#     exit 0
+# fi
+
+time=$(date +%m%d%H%M)
+i=0
+
+city="Henan_327"
+stream="rtp/225.1.4.73:1102"
+channel_key="河南联通"
+url_fofa=$(echo  '"udpxy" && country="CN" && region="Henan" && city="Zhengzhou" && protocol="http" && org="CHINA UNICOM China169 Backbone"' | base64 |tr -d '\n')
+url_fofa="https://fofa.info/result?qbase64="$url_fofa
+
+# 使用城市名作为默认文件名，格式为 CityName.ip
+ipfile="ip/${city}.ip"
+only_good_ip="ip/${city}.onlygood.ip"
+rm -f $only_good_ip
+# 搜索最新 IP
+echo "===============从 fofa 检索 ip+端口================="
+curl -o test.html "$url_fofa"
+#echo $url_fofa
+echo "$ipfile"
+grep -E '^\s*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$' test.html | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+' > "$ipfile"
+rm -f test.html
+# 遍历文件 A 中的每个 IP 地址
+while IFS= read -r ip; do
+    # 尝试连接 IP 地址和端口号，并将输出保存到变量中
+    tmp_ip=$(echo -n "$ip" | sed 's/:/ /')
+    echo "nc -w 1 -v -z $tmp_ip 2>&1"
+    output=$(nc -w 1 -v -z $tmp_ip 2>&1)
+    echo $output    
+    # 如果连接成功，且输出包含 "succeeded"，则将结果保存到输出文件中
+    if [[ $output == *"succeeded"* ]]; then
+        # 使用 awk 提取 IP 地址和端口号对应的字符串，并保存到输出文件中
+        echo "$output" | grep "succeeded" | awk -v ip="$ip" '{print ip}' >> "$only_good_ip"
+    fi
+done < "$ipfile"
+
+echo "===============检索完成================="
+
+# 检查文件是否存在
+if [ ! -f "$only_good_ip" ]; then
+    echo "错误：文件 $only_good_ip 不存在。"
+    exit 1
+fi
+
+lines=$(wc -l < "$only_good_ip")
+echo "【$only_good_ip】内 ip 共计 $lines 个"
+
+i=0
+time=$(date +%Y%m%d%H%M%S) # 定义 time 变量
+while IFS= read -r line; do
+    i=$((i + 1))
+    ip="$line"
+    url="http://$ip/$stream"
+    echo "$url"
+    curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
+    a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
+
+    echo "第 $i/$lines 个：$ip $a"
+    echo "$ip $a" >> "speedtest_${city}_$time.log"
+done < "$only_good_ip"
+
+rm -f zubo.tmp
+awk '/M|k/{print $2"  "$1}' "speedtest_${city}_$time.log" | sort -n -r >"result/result_fofa_${city}.txt"
+cat "result/result_fofa_${city}.txt"
+ip1=$(awk 'NR==1{print $2}' result/result_fofa_${city}.txt)
+ip2=$(awk 'NR==2{print $2}' result/result_fofa_${city}.txt)
+ip3=$(awk 'NR==3{print $2}' result/result_fofa_${city}.txt)
+rm -f "speedtest_${city}_$time.log"
+
+# 用 3 个最快 ip 生成对应城市的 txt 文件
+program="template/template_${city}.txt"
+
+sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
+# sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
+# sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
+cat tmp1.txt > "txt/fofa_${city}.txt"
+
+# cat tmp1.txt tmp2.txt tmp3.txt > "txt/fofa_${city}.txt"
+
+rm -rf tmp1.txt # tmp2.txt tmp3.txt
+
+
+#--------------------合并所有城市的txt文件为:   zubo_fofa.txt-----------------------------------------
+
+
+echo "河南联通,#genre#" >zubo_fofa.txt
+cat txt/Henan_327.txt >>zubo_fofa.txt
+
+
+
+# for a in result/*.txt; do echo "";echo "========================= $(basename "$a") ==================================="; cat $a; done
